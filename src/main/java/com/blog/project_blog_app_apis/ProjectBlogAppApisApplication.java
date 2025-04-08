e9e976735aa42ce96c64b9f36f35e527cbc8748a
IshitaBera14 <ishbera09@gmail.com>
2025-04-08 19:26:03 +0530

    "Blog App API"

--- src/main/java/com/blog/project_blog_app_apis/ProjectBlogAppApisApplication.java
+++ src/main/java/com/blog/project_blog_app_apis/ProjectBlogAppApisApplication.java
@@ -1,8 +1,6 @@
 package com.blog.project_blog_app_apis;
 
 import org.modelmapper.ModelMapper;
-
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
---
+++ src/main/java/com/blog/project_blog_app_apis/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.blog.project_blog_app_apis.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(); // or .formLogin() if you want login page
+
+        return http.build();
+    }
+}
