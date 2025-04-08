f22723b0c3a47e2b85032a7f5c8d0774b84ae835
IshitaBera14 <ishbera09@gmail.com>
2025-04-08 19:05:06 +0530

    "Blog App API"

--- src/main/java/com/blog/project_blog_app_apis/ProjectBlogAppApisApplication.java
+++ src/main/java/com/blog/project_blog_app_apis/ProjectBlogAppApisApplication.java
@@ -1,18 +1,17 @@
 package com.blog.project_blog_app_apis;
 
 import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.password.PasswordEncoder;
+
 
 @SpringBootApplication
-public class ProjectBlogAppApisApplication implements CommandLineRunner {
+public class ProjectBlogAppApisApplication  {
 
-	@Autowired
-	private PasswordEncoder passwordEncoder;
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectBlogAppApisApplication.class, args);
@@ -23,8 +22,5 @@ public class ProjectBlogAppApisApplication implements CommandLineRunner {
 		return new ModelMapper();
 	}
 
-	@Override
-	public void run(String... args) throws Exception {
-		System.out.println(this.passwordEncoder.encode("xyz1"));
-	}
+
 }
--- src/main/java/com/blog/project_blog_app_apis/security/JwtAuthResponse.java
+++
@@ -1,9 +0,0 @@
-package com.blog.project_blog_app_apis.security;
-
-import lombok.Data;
-
-@Data
-public class JwtAuthResponse
-{
-    private String token;
-}
